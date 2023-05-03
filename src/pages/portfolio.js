b23feeb1125cba9e9acd88425c4981b406f175c1
jamessung644 <39661528+jamessung644@users.noreply.github.com>
2023-05-03 23:52:11 +0900

    Update portfolio.js

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -69,5 +69,6 @@ export async function getServerSideProps() {
       // getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
       //revalidate: 1 // 데이터 변경이 있으면 갱신 1초 
       //러닝코어 성수한 202310671
+      //러닝코어 최혜선 202311337
     }
 }
