5cb5b77f4076db6d9a6bbabbe0e171c2d90b5f68
jamessung644 <39661528+jamessung644@users.noreply.github.com>
2023-05-03 23:54:17 +0900

    Update portfolio.js

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -68,7 +68,5 @@ export async function getServerSideProps() {
       props: {projects}, // will be passed to the page component as props
       // getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
       //revalidate: 1 // 데이터 변경이 있으면 갱신 1초 
-      //러닝코어 성수한 202310671
-      //러닝코어 최혜선 202311337
     }
 }
